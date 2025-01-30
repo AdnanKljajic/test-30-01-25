087ff617db16be9b9f2ab997cb1f867e73bd64d6
Adnan Kljajic <keljovs@gmail.com>
2025-01-30 17:27:13 +0100

    8 nezavrsen, kraj testa

---
+++ 8.js
@@ -0,0 +1,24 @@
+/*Napiši funkciju pomjeriNule(niz), koja premješta sve nule u nizu na kraj, ali zadržava redoslijed ostalih brojeva.
+
+	Ulaz: 
+
+	console.log(pomjeriNule([0, 1, 0, 3, 12]));
+console.log(pomjeriNule([0, 0, 5, 0, 7, 0, 9]));
+
+	Izlaz: 
+	
+[1, 3, 12, 0, 0]
+[5, 7, 9, 0, 0, 0, 0]
+*/
+
+function pomjeriNule(arr){
+    let index = 0;
+    arr.forEach(function(num) {
+        if (num !== 0) {
+            
+        }
+    });
+}
+
+const arr1 = [0, 1, 0, 3, 12];
+const arr2 = [0, 0, 5, 0, 7, 0, 9];

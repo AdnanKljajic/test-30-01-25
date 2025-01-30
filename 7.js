7c560fabb17b3a6e36daf34a661bbbeef32b4fa5
Adnan Kljajic <keljovs@gmail.com>
2025-01-30 17:14:28 +0100

    zad 7

--- 7.js
+++ 7.js
@@ -12,3 +12,19 @@ false
 */
 
 
+
+function jePalindrom(niz){
+    let origNiz = niz.slice();
+    let obrnutiNiz = niz.slice().reverse();
+    
+    if(origNiz.toString() === obrnutiNiz.toString()){
+        return "True";
+    }
+    else {
+        return "False";
+    }
+}
+const niz1 = [1, 2, 3, 2, 1];
+const niz2 = [1, 2, 3, 4, 5];
+console.log(jePalindrom(niz1));
+console.log(jePalindrom(niz2));
